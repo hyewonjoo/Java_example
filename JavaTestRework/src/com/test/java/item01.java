b10404e4883c8273525ed4d4541f32d503f46e66
hyewonJoo <run98@naver.com>
2022-04-06 21:46:36 +0900

    add JavaTestRework

---
+++ JavaTestRework/src/com/test/java/item01.java
@@ -0,0 +1,14 @@
+package com.test.java;
+
+public class item01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//[[SUMMARY] 변수 만들기
+		int num;
+		num = 10;
+	}
+
+}
+
